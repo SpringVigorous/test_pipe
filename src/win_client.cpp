2e045c663f5da8221101dd5ded6f352747f598c1
SpringFlourish <spring_flourish@outlook.com>
2024-09-20 22:19:52 +0800

    服务端 关闭后，客户端 异常处理

--- src/win_client.cpp
+++ src/win_client.cpp
@@ -12,11 +12,6 @@ using namespace std;
 
 int main() {
     std::cout << "命名管道通信客户端!\n";
-
-
-
-
-
     // 等待命名管道
     if (!WaitNamedPipe(pipe_name, NMPWAIT_WAIT_FOREVER)) {
         CheckLastError("Failed to wait for named pipe");
@@ -47,8 +42,8 @@ int main() {
         // 写入数据到管道
         DWORD bytesWritten;
         string input_data;
-         std::getline(std::cin, input_data);
-        // cin >> input_data;
+        //  std::getline(std::cin, input_data);
+        cin >> input_data;
 
         
 
@@ -56,7 +51,13 @@ int main() {
        
 
         if (!WriteFile(hPipe, data.c_str(), static_cast<DWORD>(data.length()), &bytesWritten, NULL)) {
-            CheckLastError("Failed to write to pipe");
+         DWORD lastError = GetLastError();
+            if (lastError == ERROR_BROKEN_PIPE or lastError== ERROR_NO_DATA) {
+                std::cerr << "服务端已关闭管道\n";
+                break;  // 退出循环
+            } else {
+                CheckLastError("Failed to write to pipe");
+            }
         }
 
         std::cout << "写入管道成功:"<< data<<std::endl;
@@ -69,7 +70,7 @@ int main() {
 
 
         // 暂停一段时间
-        this_thread::sleep_for(std::chrono::seconds(1));
+        this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 
     // 关闭管道
--- src/win_client_old.cpp
+++ src/win_client_old.cpp
@@ -9,28 +9,7 @@
 
 using namespace std;
 
-string now_stamp() {
-    // 获取当前时间点
-    auto now = std::chrono::system_clock::now();
 
-    // 将时间点转换为 time_t 类型
-    std::time_t now_c = std::chrono::system_clock::to_time_t(now);
-
-
-    // 如果需要更高精度的时间，可以使用 chrono 的 duration
-    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
-
-
-    // 组合起来输出
-    std::ostringstream stream;
-    stream << std::put_time(std::localtime(&now_c), "%Y/%m/%d %X") << "."
-        << std::setfill('0') << std::setw(3) << milliseconds.count();
-    return stream.str();
-
-
-
-
-}
 
 int main() {
     std::cout << "命名管道通信客户端!\n";
@@ -84,7 +63,7 @@ int main() {
 
 
         // 暂停一段时间
-        this_thread::sleep_for(std::chrono::seconds(1));
+        this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
     // 关闭管道
